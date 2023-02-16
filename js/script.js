2fa7e13d8d28e4a81be8a56aaf1a41b3aceab19b
unknown <Savioanjos2020@gmail.com>
2023-02-16 12:15:54 -0300

    Completando desafio 1

---
+++ js/script.js
@@ -0,0 +1,14 @@
+//Desafio 1 ---------------------------------------
+
+ const user = '{"name": "Pedro", "age": 30, "profession": "teacher" }';
+
+ const data = JSON.parse(user);
+
+ const displayData = () => {
+    console.log("Desafio 1:")
+    console.log(data)
+ }
+
+ displayData()
+
+//Desafio 1 ---------------------------------------
