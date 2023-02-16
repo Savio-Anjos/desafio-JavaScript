247d52f992cdf11b260ac8a60b40f5cbd578de6b
unknown <Savioanjos2020@gmail.com>
2023-02-16 14:35:00 -0300

    Completando desafio 2

--- js/script.js
+++ js/script.js
@@ -1,14 +1,58 @@
 //Desafio 1 ---------------------------------------
 
- const user = '{"name": "Pedro", "age": 30, "profession": "teacher" }';
+ const user = '{"nome": "Pedro", "idade": 30, "profissão": "professor" }';
 
- const data = JSON.parse(user);
+ const userData = JSON.parse(user);
 
  const displayData = () => {
     console.log("Desafio 1:")
-    console.log(data)
+    console.log(userData)
  }
 
  displayData()
 
-//Desafio 1 ---------------------------------------
+//Desafio 2 ---------------------------------------
+
+var students = [
+   {
+       id: 1,
+       nome: "Ricardo", 
+       cidade: "Jaguaquara",
+      
+      },
+      {
+       id: 2, 
+       nome: "Maria", 
+       cidade: "Belo Horizonte",
+     
+      },
+      {
+       id: 3, 
+       nome: "Carlos", 
+       cidade: "Vitória da Conquista",
+     
+      }
+]
+
+ const displayStudents = () => {
+   students.map((student) => {
+      const city = student.cidade;
+
+      if(city === "Belo Horizonte") {
+        student.estado = "MG"
+
+         
+      }
+   })
+
+   return students;
+ }
+ 
+ displayStudents()
+ console.log("Desafio 2:")
+ console.log(students)
+
+ 
+
+ 
+
