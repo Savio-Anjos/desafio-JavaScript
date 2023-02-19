8053846c1bd9da3b612fa58ce959c7503b3140dd
unknown <Savioanjos2020@gmail.com>
2023-02-19 17:38:31 -0300

    Completando desafio 1

---
+++ js/desafio-jquery.js
@@ -0,0 +1,15 @@
+//Desafio 1 ---------------------------------------
+
+const validateInput = () => {
+    const nome = $("#Nome").val();
+    const email = $("#email").val();
+    const send = $("#send").val();
+    console.log("Desafio 1:");
+    console.log(nome);
+    console.log(email);
+    console.log(send);
+    console.log("----------------------------------");
+}
+
+
+
