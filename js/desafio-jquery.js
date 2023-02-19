3f0dd9b72df34204eac9d95b34d0b300c30237b5
unknown <Savioanjos2020@gmail.com>
2023-02-19 18:08:51 -0300

    Reorganizando código

--- js/desafio-jquery.js
+++ js/desafio-jquery.js
@@ -1,14 +1,27 @@
 //Desafio 1 ---------------------------------------
 
 const validateInput = () => {
-    const nome = $("#Nome").val();
+    const name = $("#Nome").val();
     const email = $("#email").val();
     const send = $("#send").val();
+    const salary = $("#salary").val();
+    //Desafio 3 ---------------------------------------
+ // OBS: Optei por realizar o terceiro teste aqui para não ser necessário chamar duas funções
+   
+ 
+   if(name === "" || email === "" || send === "" || salary === "") {
+        console.log("Preencha todos os campos")
+    } else {
+
+   
     console.log("Desafio 1:");
-    console.log(nome);
+    console.log(name);
     console.log(email);
     console.log(send);
     console.log("----------------------------------");
+    }
+
+    
 }
 
 //Desafio 2 ---------------------------------------
@@ -22,4 +35,8 @@ const verifyCheckbox = () => {
     console.log("O usuário não deseja ser alertado!")
    }
 }
+
+
+
+
 
